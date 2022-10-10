f38f1f3a7db2f271bd45e01e387f4987feb11471
bmul0006 <108573936+bmul0006@users.noreply.github.com>
2022-10-10 21:52:23 +1100

    Added top 5 bar chart

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,8 +1,12 @@
 var vg_1 = "choropleth_graticules.vega";
 var vg_scatter = "scatterplot.vega";
+var vg_bar = "barchart.vega";
 vegaEmbed("#choropleth_map", vg_1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 vegaEmbed("#scatterplot", vg_scatter).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+vegaEmbed("#barchart", vg_bar).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
