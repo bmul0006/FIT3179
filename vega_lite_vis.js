e038b8b21d59ab78f78cdbe5024259cc571aac92
bmul0006 <108573936+bmul0006@users.noreply.github.com>
2022-10-10 23:19:14 +1100

    Added stream graph

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,6 +1,7 @@
 var vg_1 = "choropleth_graticules.vega";
 var vg_scatter = "scatterplot.vega";
 var vg_bar = "barchart.vega";
+var vg_stream = "streamgraph.vega";
 vegaEmbed("#choropleth_map", vg_1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
@@ -8,5 +9,8 @@ vegaEmbed("#scatterplot", vg_scatter).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 vegaEmbed("#barchart", vg_bar).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+vegaEmbed("#streamgraph", vg_stream).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
