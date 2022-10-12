b01b5a0527e7597608c9ee1ece1179ed88d1dab7
bmul0006 <108573936+bmul0006@users.noreply.github.com>
2022-10-12 17:47:28 +1100

    Added line chart

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -2,6 +2,8 @@ var vg_1 = "choropleth_graticules.vega";
 var vg_scatter = "scatterplot.vega";
 var vg_bar = "barchart.vega";
 var vg_stream = "streamgraph.vega";
+var vg_line = "linechart.vega";
+
 vegaEmbed("#choropleth_map", vg_1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
@@ -12,5 +14,8 @@ vegaEmbed("#barchart", vg_bar).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 vegaEmbed("#streamgraph", vg_stream).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+vegaEmbed("#linechart", vg_line).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
